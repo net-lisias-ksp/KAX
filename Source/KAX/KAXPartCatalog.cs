b1294a78e4ed10b39670e378d540bb9bbdbe1735
Lisias T <me@lisias.net>
2019-01-01 10:31:17 -0200

    Adding icons to the Category Filters on the Advanced Menu

--- Source/KAX/KAXPartCatalog.cs
+++ Source/KAX/KAXPartCatalog.cs
@@ -19,7 +19,7 @@ namespace KAX
 
 		public void Awake()
 		{
-			this.icon = GenIcons(Constants.PLUGIN_ID);
+			this.icon = GenIcons("KAX");
 //			GameEvents.onGUIEditorToolbarReady.Add(addSimpleMenufilter);
 			GameEvents.onGUIEditorToolbarReady.Add(addAdvMenufilter);
 		}
@@ -49,49 +49,63 @@ namespace KAX
 
 		private void SubCategories()
 		{
-			if (null == this.icon) return;
 			PartCategorizer.Category filter = PartCategorizer.Instance.filters.Find(f => f.button.categorydisplayName == "#autoLOC_453547");//change for 1.3.1
-//			PartCategorizer.AddCustomSubcategoryFilter(filter, Constants.PLUGIN_ID, Constants.PLUGIN_ID, icon, EditorItemsFilter);
-			PartCategorizer.AddCustomSubcategoryFilter(filter, Constants.PLUGIN_ID, Constants.PLUGIN_ID, icon, p => parts.Contains(p));
+//			PartCategorizer.AddCustomSubcategoryFilter(filter, Constants.PLUGIN_ID, Constants.PLUGIN_ID, this.icon, EditorItemsFilter);
+			PartCategorizer.AddCustomSubcategoryFilter(filter, Constants.PLUGIN_ID, Constants.PLUGIN_ID, this.icon, p => parts.Contains(p));
 		}
 
 		private Icon GenIcons(string iconName)
 		{
-			Texture2D normIcon = this.GenIconTexture("normal");
-			Texture2D selIcon = this.GenIconTexture("selected");
-
-			return new Icon(iconName + "Icon", normIcon, selIcon);
+			try
+			{
+				Texture2D normIcon = this.GenIconTexture(iconName + "_Unselected");
+				Texture2D selIcon = this.GenIconTexture(iconName + "_Selected");
+	
+				return new Icon(iconName + "Icon", normIcon, selIcon);
+			}
+			catch (Exception e)
+			{
+				Debug.LogException(e);
+				return this.icon;
+			}
 		}
 
 		private Texture2D GenIconTexture(string iconName)
 		{
 			Texture2D r = new Texture2D(64, 64, TextureFormat.RGBA32, false);
-			string filename = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/PluginData", "KAX_" + iconName + ".png"); // icon to be present in same folder as dll
+			string filename = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/PluginData", iconName + ".png"); // icon to be present in same folder as dll
 			r.LoadImage(File.ReadAllBytes(filename));
 			return r;
 		}
 
 
-		private void addAdvMenufilter()
+		private void GenFilter(PartCategorizer.Category filter, string subFilterName, string subFilterdisplayName, string iconName, Func<AvailablePart, bool> exclusionFilter)
 		{
-			if (null == this.icon) return;
-
 			try
 			{
-				PartCategorizer.Category filter = PartCategorizer.AddCustomFilter(Constants.PLUGIN_ID, Constants.MANUFACTURER_NAME, this.icon, Color.white);
-
-				PartCategorizer.AddCustomSubcategoryFilter(filter, "AllParts", string.Format("All {0} Parts", Constants.MANUFACTURER_NAME), this.icon, o => o.manufacturer == Constants.MANUFACTURER_NAME && !o.title.Contains("(LEGACY)"));
-				PartCategorizer.AddCustomSubcategoryFilter(filter, "CommandPods", "#autoLOC_453549", this.icon, o => o.manufacturer == Constants.MANUFACTURER_NAME && o.category.ToString() == "Pods" && !o.title.Contains("(LEGACY)"));
-				PartCategorizer.AddCustomSubcategoryFilter(filter, "Tanks", "#autoLOC_453552", this.icon, p => p.manufacturer == Constants.MANUFACTURER_NAME && !p.title.Contains("(LEGACY)") && p.resourceInfos.Exists(q => q.resourceName == "LiquidFuel" || q.resourceName == "Oxidizer" || q.resourceName == "Monopropellant") );
-				PartCategorizer.AddCustomSubcategoryFilter(filter, "Structural", "#autoLOC_453561", this.icon, o => o.manufacturer == Constants.MANUFACTURER_NAME && !o.title.Contains("(LEGACY)") && o.category.ToString() == "Structural" );
-//				PartCategorizer.AddCustomSubcategoryFilter(filter, "Electrical", "#autoLOC_453579", this.icon, o => o.manufacturer == Constants.MANUFACTURER_NAME && !o.title.Contains("(LEGACY)") && o.category.ToString() == "Electrical" );
-				PartCategorizer.AddCustomSubcategoryFilter(filter, "Engines", "#autoLOC_453555", this.icon, o => o.manufacturer == Constants.MANUFACTURER_NAME && !o.title.Contains("(LEGACY)") && o.moduleInfos.Exists(q => q.moduleName == "Engine") );
-//				PartCategorizer.AddCustomSubcategoryFilter(filter, "Legacy", "#autoLOC_1900223", this.icon, o => o.manufacturer == Constants.MANUFACTURER_NAME && o.title.Contains("(LEGACY)"));			
+				Icon icon = GenIcons(iconName);
+				PartCategorizer.AddCustomSubcategoryFilter(filter, subFilterName, subFilterdisplayName, icon, exclusionFilter);
 			}
 			catch (Exception e)
 			{
 				Debug.LogException(e);
 			}
+
+		}
+		
+		private void addAdvMenufilter()
+		{
+			// TODO Better add'on Icons!
+			PartCategorizer.Category filter = PartCategorizer.AddCustomFilter(Constants.PLUGIN_ID, Constants.MANUFACTURER_NAME, GenIcons("KAX"), Color.white);
+
+			// TODO Better "All Parts" icons. Localization for it too!
+			GenFilter(filter, "AllParts", string.Format("All {0} Parts", Constants.MANUFACTURER_NAME), "KAX", o => o.manufacturer == Constants.MANUFACTURER_NAME && !o.title.Contains("(LEGACY)"));
+			GenFilter(filter, "CommandPods", "#autoLOC_453549", "Pods", o => o.manufacturer == Constants.MANUFACTURER_NAME && o.category.ToString() == "Pods" && !o.title.Contains("(LEGACY)"));
+			GenFilter(filter, "Tanks", "#autoLOC_453552", "Tanks", p => p.manufacturer == Constants.MANUFACTURER_NAME && !p.title.Contains("(LEGACY)") && p.resourceInfos.Exists(q => q.resourceName == "LiquidFuel" || q.resourceName == "Oxidizer" || q.resourceName == "Monopropellant") );
+			GenFilter(filter, "Structural", "#autoLOC_453561", "Structural", o => o.manufacturer == Constants.MANUFACTURER_NAME && !o.title.Contains("(LEGACY)") && o.category.ToString() == "Structural" );
+//			GenFilter(filter, "Electrical", "#autoLOC_453579", this.icon, o => o.manufacturer == Constants.MANUFACTURER_NAME && !o.title.Contains("(LEGACY)") && o.category.ToString() == "Electrical" );
+			GenFilter(filter, "Engines", "#autoLOC_453555", "Engines", o => o.manufacturer == Constants.MANUFACTURER_NAME && !o.title.Contains("(LEGACY)") && o.moduleInfos.Exists(q => q.moduleName == "Engine") );
+//			GenFilter(filter, "Legacy", "#autoLOC_1900223", this.icon, o => o.manufacturer == Constants.MANUFACTURER_NAME && o.title.Contains("(LEGACY)"));			
 		}
 
 
