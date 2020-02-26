f8d28100fc4ea824baae756fb2c38e31f8c83299
Lisias <me@lisias.net>
2020-02-25 21:52:39 -0300

    Legal Mambo Jambo

--- Source/KAX/Properties/AssemblyInfo.cs
+++ Source/KAX/Properties/AssemblyInfo.cs
@@ -1,4 +1,26 @@
-﻿using System.Reflection;
+﻿/*
+	This file is part of "KAX DLL"
+	(C) 2018-2020 Lisias T : http://lisias.net <support@lisias.net>
+
+	"KAX DLL" is double licensed, as follows:
+
+	* SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
+	* GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+
+	And you are allowed to choose the License that better suit your needs.
+
+	"KAX DLL" is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the SKL Standard License 1.0
+	along with "KAX DLL". If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with "KAX DLL". If not, see <https://www.gnu.org/licenses/>.
+
+*/
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,8 +32,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("L Aerospace KSP Division")]
 [assembly: AssemblyProduct("KAX Categorizer")]
-[assembly: AssemblyCopyright("Copyright ©  2018-2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2018-2020 LisiasT; 2017-2018 SpannerMonkey; 2014-2026 Keptin")]
+[assembly: AssemblyTrademark("KAX")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
